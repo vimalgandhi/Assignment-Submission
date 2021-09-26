4ab82e01458dc2f3727de1f282697232d52b02b2
Vims1012 <65559455+vimalgandhi@users.noreply.github.com>
2021-09-26 13:41:31 +0530

    day-1

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Vimal from './Vimal';
+function Welcome(){
+  return <h1>vijal</h1>
+}
+
+function App() {
+  return (
+    <>
+  <h1>vimal Gandhi</h1>
+  <Welcome />
+  <Vimal/>
+  </>
+  );
+}
+
+export default App;
---
+++ Vimal.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Vimal extends Component {
+    render() {
+        return (
+            <div>
+                <h1>let's upgrade</h1>
+            </div>
+        )
+    }
+}
